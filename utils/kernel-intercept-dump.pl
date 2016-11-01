ebbd942ad00b3a4eb74fbfcd915e82a263243d02
Richard Fuchs <rfuchs@sipwise.com>
2016-11-01 14:14:08 -0400

    support writing pcap files in reference intercept script
    Change-Id: I53502ba416426e9012a68c35cdf4b457d7c3eb69

--- utils/kernel-intercept-dump.pl
+++ utils/kernel-intercept-dump.pl
@@ -7,6 +7,8 @@ use AnyEvent::Loop;
 use AnyEvent;
 use Fcntl;
 use Errno qw(EINTR EIO EAGAIN EWOULDBLOCK :POSIX);
+use Net::Pcap;
+use Time::HiRes;
 
 my $i = new Linux::Inotify2 or die;
 $i->blocking(0);
@@ -76,10 +78,8 @@ sub handle_delete {
 	print("handling delete on $fn\n");
 
 	for my $sn (keys(%{$mf->{streams}})) {
-		my $stream = $mf->{streams}->{$sn};
-		# this needs to be done explicitly, otherwise the closure would keep
-		# the object from being freed
-		delete($stream->{watcher});
+		my $ref = $mf->{streams}->{$sn};
+		close_stream($ref);
 	}
 
 	delete($mf->{streams});
@@ -92,15 +92,29 @@ sub open_stream {
 	print("opening $stream for $mf->{'CALL-ID'}\n");
 	my $fd;
 	sysopen($fd, '/proc/rtpengine/0/calls/' . $mf->{PARENT} . '/' . $stream, O_RDONLY | O_NONBLOCK) or return;
-	my $ref = { name => $stream, fh => $fd };
+	my $ref = { name => $stream, fh => $fd, metafile => $mf };
 	$ref->{watcher} = AnyEvent->io(fh => $fd, poll => 'r', cb => sub { stream_read($mf, $ref) });
+	$ref->{pcap} = pcap_open_dead(DLT_RAW, 65535);
+	$ref->{dumper} = pcap_dump_open($ref->{pcap}, $mf->{PARENT} . '-' . $stream . '.pcap');
 	$mf->{streams}->{$stream} = $ref;
 	print("opened for reading $stream for $mf->{'CALL-ID'}\n");
 }
 
+sub close_stream {
+	my ($ref) = @_;
+	# this needs to be done explicitly, otherwise the closure would keep
+	# the object from being freed
+	delete($ref->{watcher});
+	my $mf = $ref->{metafile};
+	delete($mf->{streams}->{$ref->{name}});
+	pcap_dump_close($ref->{dumper});
+	pcap_close($ref->{pcap});
+	print("closed $ref->{name}\n");
+}
+
 sub stream_read {
 	my ($mf, $ref) = @_;
-	print("handling read event for $mf->{name} / $ref->{name}\n");
+	#print("handling read event for $mf->{name} / $ref->{name}\n");
 	while (1) {
 		my $buf;
 		my $ret = sysread($ref->{fh}, $buf, 65535);
@@ -117,13 +131,22 @@ sub stream_read {
 		}
 		else {
 			# $ret > 0
-			print("$ret bytes read from $ref->{name} for $mf->{'CALL-ID'}\n");
+			#print("$ret bytes read from $ref->{name} for $mf->{'CALL-ID'}\n");
+			my $hdr = { len => $ret, caplen => $ret };
+			tvsec_now($hdr);
+			pcap_dump($ref->{dumper}, $hdr, $buf);
 			next;
 		}
 
 		# some kind of error
-		delete($ref->{watcher});
-		delete($mf->{streams}->{$ref->{name}});
+		close_stream($ref);
 		return;
 	}
+}
+
+sub tvsec_now {
+	my ($h) = @_;
+	my @now = Time::HiRes::gettimeofday();
+	$h->{tv_sec} = $now[0];
+	$h->{tv_usec} = $now[1];
 }
